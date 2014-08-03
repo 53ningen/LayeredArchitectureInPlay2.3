acc414f7614129fd6bafae5c9c721f9845106826
53nigen <korun24276@gmail.com>
2014-08-03 23:09:28 +0900

    ルーティングまわりをいじった

---
+++ modules/layered-application/app/controllers/Assets.scala
@@ -0,0 +1,3 @@
+package controllers.application
+
+object Assets extends controllers.AssetsBuilder
--- modules/layered-application/app/jp.hoge.layered.application/controllers/Application.scala
+++ modules/layered-application/app/jp.hoge.layered.application/controllers/Application.scala
@@ -1,4 +1,4 @@
-package controllers
+package jp.hoge.layered.application.controllers
 
 import play.api._
 import play.api.mvc._
@@ -6,7 +6,7 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("this is a test page."))
   }
 
 }
